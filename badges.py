b8a36c38e85ad92c2e2fbdc400130f60067c5726
Steve Calnan <asciimo@gmail.com>
2020-03-01 20:39:05 -0800

    Place employee photos on a copy of the badge image, and save to sensible file names.

--- badges.py
+++ badges.py
@@ -6,12 +6,20 @@ def load_employees(path):
         data = json.load(f)
         return data.get("employees")
 
+def get_badge_filename(employee):
+    raw_filename = "%s_%s_%s.png" % (employee.get("id"), employee.get("last_name"), employee.get("first_name"))
+    return "".join(c for c in raw_filename if (c.isalnum() or c in "_.")).lower()
+
 def main():
+    template = Image.open("src/badge.png")
     employees = load_employees("src/employees.json")
     for employee in employees:
-        print("%s %s" % (employee.get("first_name"), employee.get("last_name")))
-        im = Image.open("src/%s" % employee.get("photo"))
-        im.save("dist/%s" % employee.get("photo").replace(".jpg", ".png"))
+        print("Processing %s %s" % (employee.get("first_name"), employee.get("last_name")))
+        output_filename = get_badge_filename(employee)
+        employee_badge = template.copy()
+        employee_photo = Image.open("src/%s" % employee.get("photo"))
+        employee_badge.paste(employee_photo, (185, 215))
+        employee_badge.save("dist/%s" % output_filename)
 
 if __name__ == "__main__":
     main()
