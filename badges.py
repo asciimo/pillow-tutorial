6e2670263cb239f065237b540e7670e04be7b23e
Steve Calnan <asciimo@gmail.com>
2020-03-01 19:14:38 -0800

    Get photo path from employees.json, print the employee name, and transform their photo JPGs to PNGs

--- badges.py
+++ badges.py
@@ -1,8 +1,17 @@
 from PIL import Image
+import json
+
+def load_employees(path):
+    with open(path) as f:
+        data = json.load(f)
+        return data.get("employees")
 
 def main():
-    im = Image.open("src/badge.png")
-    im.save("dist/badge_out.png")
+    employees = load_employees("src/employees.json")
+    for employee in employees:
+        print("%s %s" % (employee.get("first_name"), employee.get("last_name")))
+        im = Image.open("src/%s" % employee.get("photo"))
+        im.save("dist/%s" % employee.get("photo").replace(".jpg", ".png"))
 
 if __name__ == "__main__":
     main()
